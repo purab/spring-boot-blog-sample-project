01ec6e261a912d1d6bbd27b0ca489ee7f04bdc72
purab <purabdk@yahoo.co.in>
2021-11-13 16:45:55 +0530

    user save api added...

---
+++ src/main/java/in/purabtech/service/AuthService.java
@@ -0,0 +1,49 @@
+package in.purabtech.service;
+
+import in.purabtech.dto.RegisterRequest;
+import in.purabtech.model.User;
+import in.purabtech.model.VerificationToken;
+import in.purabtech.repository.UserRepository;
+import in.purabtech.repository.VerificationTokenRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.time.Instant;
+import java.util.UUID;
+
+@Service
+@AllArgsConstructor
+public class AuthService {
+
+    private final PasswordEncoder passwordEncoder;
+    private final UserRepository userRepository;
+    private final VerificationTokenRepository verificationTokenRepository;
+
+    @Transactional
+    public void signup(RegisterRequest registerRequest) {
+        User user = new User();
+        user.setEmail(registerRequest.getEmail());
+        user.setUsername(registerRequest.getUsername());
+        user.setPassword(passwordEncoder.encode(registerRequest.getPassword()));
+        user.setCreated(Instant.now());
+        user.setEnabled(false);
+
+        //save user
+        userRepository.save(user);
+        String token = generateVerificationToken(user);
+    }
+
+    private String generateVerificationToken(User user) {
+        String token = UUID.randomUUID().toString();
+        VerificationToken verificationToken = new VerificationToken();
+        verificationToken.setToken(token);
+        verificationToken.setUser(user);
+
+        verificationTokenRepository.save(verificationToken);
+        return token;
+
+    }
+}
